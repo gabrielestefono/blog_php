ebb6dafa427b8da0606df638f1d1c10b0c9b653e
gabrielestefono <gabrielestefono@hotmail.com>
2024-12-03 06:16:32 -0300

    Criando componente de Post

---
+++ src/Components/Shared/Post.php
@@ -0,0 +1,20 @@
+<div class="post-card">
+    <div>
+        <img src="/public/assets/Examples/post-image.png" alt="Testando">
+    </div>
+    <div>
+        <div>
+            <span>Technology</span>
+        </div>
+        <div>
+            <h2>
+                The Impact of Technology on the Workplace: How Technology is Changing
+            </h2>
+        </div>
+        <div>
+            <div><img src="/public/assets/Examples/avatar.png" alt="Testando"></div>
+            <span>Jason Francisco</span>
+            <span>August 20, 2022</span>
+        </div>
+    </div>
+</div>
--- src/Views/Pages/index.php
+++ src/Views/Pages/index.php
@@ -3,4 +3,5 @@
 use App\Helpers\View;
 
 View::component('./Pages/Dashboard/Destaque/Destaque.php');
+View::component('./Shared/Post.php');
 View::view('../Layout/Visitor.php', ["titulo" => "WeBest - Início"]);
