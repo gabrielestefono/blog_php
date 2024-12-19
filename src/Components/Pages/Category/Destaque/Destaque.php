559fe1589619bccbd6da5885feb426082db49bf3
gabrielestefono <gabrielestefono@hotmail.com>
2024-12-19 08:46:55 -0300

    Criando destaque da página de categorias

---
+++ src/Components/Pages/Category/Destaque/Destaque.php
@@ -0,0 +1,13 @@
+<div class="category-highlight">
+    <img src="public/Examples/destaque-categoria.png" alt="Destaque Categoria">
+    <div>
+        <h2>Technology</h2>
+        <h3>The Impact of Technology on the Workplace: How Technology is Changing</h3>
+        <div>
+            <img src="public/Examples/avatar.png" alt="">
+            <span>Tracey Wilson</span>
+            <span>August 20, 2022</span>
+        </div>
+    </div>
+</div>
+
--- src/Views/Pages/category.php
+++ src/Views/Pages/category.php
@@ -3,4 +3,5 @@
 use App\Helpers\View;
 
 View::componentChild('./Pages/Category/Title/Title.php');
+View::componentChild('./Pages/Category/Destaque/Destaque.php');
 View::view('../Layout/Visitor.php', ["titulo" => "WeBest - Categorias"]);
