c5ad4bfda543992f19e151f317ba6c0452f3ce30
gabrielestefono <gabrielestefono@hotmail.com>
2024-12-20 23:31:39 -0300

    Criando Página de Últimos Posts em Categorias

---
+++ src/Components/Pages/Category/LatestPosts/LatestPosts.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Helpers\View;
+
+?>
+
+<section class="category-latest-posts">
+    <div>
+        <?php View::component('/Shared/Post.php'); ?>
+        <?php View::component('/Shared/Post.php'); ?>
+        <?php View::component('/Shared/Post.php'); ?>
+        <?php View::component('/Shared/Post.php'); ?>
+        <?php View::component('/Shared/Post.php'); ?>
+        <?php View::component('/Shared/Post.php'); ?>
+        <?php View::component('/Shared/Post.php'); ?>
+        <?php View::component('/Shared/Post.php'); ?>
+        <?php View::component('/Shared/Post.php'); ?>
+    </div>
+</section>
--- src/Views/Pages/category.php
+++ src/Views/Pages/category.php
@@ -4,4 +4,5 @@ use App\Helpers\View;
 
 View::componentChild('./Pages/Category/Title/Title.php');
 View::componentChild('./Pages/Category/Destaque/Destaque.php');
+View::componentChild('./Pages/Category/LatestPosts/LatestPosts.php');
 View::view('../Layout/Visitor.php', ["titulo" => "WeBest - Categorias"]);
