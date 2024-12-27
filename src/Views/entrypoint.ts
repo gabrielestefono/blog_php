8f5cd9937a80095604f3b3eaa14248a75a1b5153
gabrielestefono <gabrielestefono@hotmail.com>
2024-12-27 12:09:14 -0300

    Modificando css e js

--- src/Views/entrypoint.ts
+++ src/Views/entrypoint.ts
@@ -1,5 +1,6 @@
 import { Header } from "../Components/Layout/Visitor/Header/Header";
 
 addEventListener("DOMContentLoaded", () => {
+  // TODO: Fazer uma verificação se é admin ou visitante
   new Header(); // NOSONAR
 });
