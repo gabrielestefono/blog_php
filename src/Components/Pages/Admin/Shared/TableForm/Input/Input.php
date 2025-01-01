65c72c960c0876fc3e2aee27d3e5994561e32a3d
gabrielestefono <gabrielestefono@hotmail.com>
2025-01-01 12:10:32 -0300

    Atualizando Projeto

---
+++ src/Components/Pages/Admin/Shared/TableForm/Input/Input.php
@@ -0,0 +1,6 @@
+<div class="<?php echo (bool) $halfSize ? 'col-sm-6' : 'col-sm-12'; ?>">
+    <div class="form-group">
+        <label for="<?php echo $name; ?>"><?php echo $label; ?></label>
+        <input type="text" class="form-control" placeholder="<?php echo $placeholder; ?>" name="<?php echo $name; ?>">
+    </div>
+</div>
